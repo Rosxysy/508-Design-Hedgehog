3409a2606cf71c6eaeb49d7d27716193af0c7fe1
Rosxysy <vmk24169@my.acm.ac.uk>
2025-11-06 16:51:13 +0000

    Movement and Camera
    Updated the player movement so it works with the free look camera. Pressing E to switch between rotations, so walking to rolling.

--- Assets/Scripts/MovingSphere.cs
+++ Assets/Scripts/MovingSphere.cs
@@ -1,46 +1,98 @@
-using UnityEditor.Callbacks;
 using UnityEngine;
 
 public class MovingSphere : MonoBehaviour
 {
-
     [SerializeField, Range(0f, 100f)]
-	float maxSpeed = 10f;
+    float maxSpeed = 10f;
+    [SerializeField, Range(0f, 100f)] 
     float maxAcceleration = 10f;
+    
     private Rigidbody rb;
-    private Vector3 velocity;
+    private Vector3 currentVelocity; 
+
+    [SerializeField] 
+    public Transform cameraTransform;
+
     void Awake()
     {
         rb = GetComponent<Rigidbody>();
-        velocity = rb.linearVelocity;
+
+
+        if (cameraTransform == null && Camera.main != null)
+            cameraTransform = Camera.main.transform;
     }
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
-
     void Update()
     {
-
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            ToggleRotationContrainsts();
+        }
     }
     void FixedUpdate()
     {
         Vector2 playerInput;
         playerInput.x = Input.GetAxis("Horizontal");
         playerInput.y = Input.GetAxis("Vertical");
-
         playerInput = Vector2.ClampMagnitude(playerInput, 1f);
-        Vector3 desiredVelocity = new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+
+
+        if (rb == null || cameraTransform == null)
+            return;
+
+
+        Vector3 camForward = cameraTransform.forward;
+        Vector3 camRight = cameraTransform.right;
+
+
+        camForward.y = 0f;
+        camRight.y = 0f;
+        camForward.Normalize();
+        camRight.Normalize();
+
+
+        Vector3 desiredVelocity = (camRight * playerInput.x + camForward * playerInput.y) * maxSpeed;
+
+
+        currentVelocity = rb.linearVelocity;
+
         float maxSpeedChange = maxAcceleration * Time.deltaTime;
-        velocity.x =
-            Mathf.MoveTowards(velocity.x, desiredVelocity.x, maxSpeedChange);
-        velocity.z =
-            Mathf.MoveTowards(velocity.z, desiredVelocity.z, maxSpeedChange);
-                
-        rb.linearVelocity = new Vector3(velocity.x, rb.linearVelocity.y, velocity.z);
 
 
+        currentVelocity.x = Mathf.MoveTowards(
+            currentVelocity.x,
+            desiredVelocity.x,
+            maxSpeedChange
+        );
+        currentVelocity.z = Mathf.MoveTowards(
+            currentVelocity.z,
+            desiredVelocity.z,
+            maxSpeedChange
+        );
+
+
+        rb.linearVelocity = currentVelocity;
     }
     
+
+
+    private void ToggleRotationContrainsts ()
+    {
+        if (rb == null) return;
+
+        bool isRotationFrozen = (rb.constraints & RigidbodyConstraints.FreezeRotation) == RigidbodyConstraints.FreezeRotation;
+
+        if (isRotationFrozen)
+        {
+            rb.constraints &= ~RigidbodyConstraints.FreezeRotation;
+        }
+        else
+        {
+            rb.constraints |= RigidbodyConstraints.FreezeRotation;
+        }
+    }
 }
